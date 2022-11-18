73c9fff691c44f153e4935d10930635261c5a4b2
dest316 <90666585+dest316@users.noreply.github.com>
2022-11-18 15:01:54 +1000

    Add files via upload

---
+++ MainWindow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using SpaceVIL;
+
+namespace InterfacePrototype
+{
+    internal class MainWindow: ActiveWindow
+    {
+        public override void InitWindow()
+        {
+            SetParameters(nameof(MainWindow), nameof(MainWindow), 800, 600);
+            SetMinSize(400, 300);
+            SetBackground(32, 34, 37);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using SpaceVIL;
+using System;
+
+namespace InterfacePrototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SpaceVIL.Common.CommonService.InitSpaceVILComponents();
+            MainWindow mw = new MainWindow();
+            mw.Show();
+        }
+    }
+}
